9f06866e9ef5928fedf2cd51af412daa45c1bef6
mekmiotek <mekmiotek@gmail.com>
2023-12-18 21:20:54 -0500

    Create standard_wcolors.ps1

---
+++ standard_wcolors.ps1
@@ -0,0 +1,21 @@
+# Set Powershell Prompt
+function prompt {
+  $currentuser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+  $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+  $principal = [Security.Principal.WindowsPrincipal] $identity
+  $adminRole = [Security.Principal.WindowsBuiltInRole]::Administrator
+
+  # ANSI escape codes for color formatting
+  $blue = "`e[34m"
+  $green = "`e[32m"
+  $yellow = "`e[33m"
+  $resetColor = "`e[0m"
+
+  $(if (Test-Path variable:/PSDebugContext) { "$($blue)[DBG]: $($resetColor)" }
+    elseif($principal.IsInRole($adminRole)) { "$($green)[ADMIN]: $($resetColor)" }
+    else { '' }
+  ) + "$($blue)[$(Get-Date -f 'hh:mm:ss tt')] $($resetColor)" +
+    "$($green)$currentuser $($resetColor)" +
+    "$($yellow)$(Get-Location) $($resetColor)" +
+    $(if ($NestedPromptLevel -ge 1) { '>>' }) + '> '
+}
