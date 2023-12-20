8a5844ddcd25afb19499aad60259696d87811129
mekmiotek <mekmiotek@gmail.com>
2023-12-19 20:33:45 -0500

    Create american_flag.ps1

---
+++ american_flag.ps1
@@ -0,0 +1,21 @@
+# Set Powershell Prompt
+function prompt {
+    $currentuser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+    $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+    $principal = [Security.Principal.WindowsPrincipal] $identity
+    $adminRole = [Security.Principal.WindowsBuiltInRole]::Administrator
+
+    # ANSI escape codes for color formatting
+    $blue = "`e[34m"
+    $white = "`e[97m"
+    $red = "`e[91m"
+    $resetColor = "`e[0m"
+
+    $(if (Test-Path variable:/PSDebugContext) { "$($blue)[DBG]: $($resetColor)" }
+        elseif($principal.IsInRole($adminRole)) { "$($red)[ADMIN]: $($resetColor)" }
+        else { '' }
+    ) + "$($white)$(Get-Date -f 'MM/dd/yyyy') $($blue)$(Get-Date -f 'hh:mm:ss tt') $($resetColor)" +
+        "$($red)$currentuser $($resetColor)" +
+        "$($white)$(Get-Location) $($resetColor)" +
+        $(if ($NestedPromptLevel -ge 1) { "$($blue)>>$($resetColor)" }) + "$(blue)$('> ')"
+}
